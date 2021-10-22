ca0a2ca47e870ca35f04b4425685190d13162dde
Alexandre Dos Santos Goncalves <alexandre.sgoncalves@mrv.com.br>
2021-10-22 16:19:53 -0300

    javaScript 03-Array, Loop e Estilos - 01-Replicando a validação e o cálculo do IMC para todos os pacientes

--- javaScript/introducao-javascript/js/principal.js
+++ javaScript/introducao-javascript/js/principal.js
@@ -6,31 +6,41 @@ console.log(titulo.textContent);
 
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var peso = paciente.querySelector(".info-peso").textContent;
-var altura = paciente.querySelector(".info-altura").textContent;
-var imc = paciente.querySelector(".info-imc");
+//console.log(pacientes);
 
-var validacaoIMC = true;
+for (var i = 0; i < pacientes.length; i++) {
 
-if(peso <= 0 || peso >= 1000){
-    var st = "Peso inválido";
-    console.log(st);
-    validacaoIMC = false;
-    imc.textContent = st;
+    var paciente = pacientes[i];
+
+    var peso = paciente.querySelector(".info-peso").textContent;
+    var altura = paciente.querySelector(".info-altura").textContent;
+    var imc = paciente.querySelector(".info-imc");
+
+    var validacaoIMC = true;
+
+    if (peso <= 0 || peso >= 1000) {
+        var st = "Peso inválido";
+        console.log(st);
+        validacaoIMC = false;
+        imc.textContent = st;
+    }
+
+    if (altura <= 0 || altura >= 3.0) {
+        var st = "Altura inválido";
+        console.log(st);
+        validacaoIMC = false;
+        imc.textContent = st;
+    }
+
+    if (validacaoIMC) {
+        var arr =  peso / (altura * altura);
+        imc.textContent = arr.toFixed(2);
+    }
 }
 
-if(altura <= 0 || altura >= 3.0){
-    var st = "Altura inválido";
-    console.log(st);
-    validacaoIMC = false;
-    imc.textContent = st;
-}
 
-if(validacaoIMC){
-    imc.textContent = peso / (altura * altura);
-}
 
 
 
