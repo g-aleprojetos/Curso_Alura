a32e0d3b5300aafcf0f02f4fe14a7143dd542d05
Alexandre Dos Santos Goncalves <alexandre.sgoncalves@mrv.com.br>
2021-10-22 16:35:18 -0300

    javaScript 03-Array, Loop e Estilos - 02-Estilos com JavaScript

--- javaScript/introducao-javascript/js/principal.js
+++ javaScript/introducao-javascript/js/principal.js
@@ -25,6 +25,7 @@ for (var i = 0; i < pacientes.length; i++) {
         console.log(st);
         validacaoIMC = false;
         imc.textContent = st;
+        paciente.classList.add("paciente-invalido");
     }
 
     if (altura <= 0 || altura >= 3.0) {
@@ -32,10 +33,12 @@ for (var i = 0; i < pacientes.length; i++) {
         console.log(st);
         validacaoIMC = false;
         imc.textContent = st;
+        paciente.classList.add("paciente-invalido");
+
     }
 
     if (validacaoIMC) {
-        var arr =  peso / (altura * altura);
+        var arr = peso / (altura * altura);
         imc.textContent = arr.toFixed(2);
     }
 }
