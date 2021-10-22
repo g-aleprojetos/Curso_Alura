c0b01262587f9f17ba09e0ec703c3db0d7ccf153
Alexandre Dos Santos Goncalves <alexandre.sgoncalves@mrv.com.br>
2021-10-22 15:43:36 -0300

    javaScript 02-Variaveis e operadores - 07-Operadores Lógicos

--- javaScript/introducao-javascript/js/principal.js
+++ javaScript/introducao-javascript/js/principal.js
@@ -10,7 +10,31 @@ var paciente = document.querySelector("#primeiro-paciente");
 
 var peso = paciente.querySelector(".info-peso").textContent;
 var altura = paciente.querySelector(".info-altura").textContent;
-paciente.querySelector(".info-imc").textContent = peso / (altura * altura);
+var imc = paciente.querySelector(".info-imc");
+
+var validacaoIMC = true;
+
+if(peso <= 0 || peso >= 1000){
+    var st = "Peso inválido";
+    console.log(st);
+    validacaoIMC = false;
+    imc.textContent = st;
+}
+
+if(altura <= 0 || altura >= 3.0){
+    var st = "Altura inválido";
+    console.log(st);
+    validacaoIMC = false;
+    imc.textContent = st;
+}
+
+if(validacaoIMC){
+    imc.textContent = peso / (altura * altura);
+}
+
+
+
+
 
 
 
