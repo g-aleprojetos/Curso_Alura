cec20596473f9521aba6e5cc142499ec1d1d206b
Alexandre Dos Santos Goncalves <alexandre.sgoncalves@mrv.com.br>
2021-10-22 14:37:17 -0300

    javaScript 02-Variáveis e Operadores - 01-Buscando dados do paciente

--- javaScript/introducao-javascript/js/principal.js
+++ javaScript/introducao-javascript/js/principal.js
@@ -1,8 +1,18 @@
 
-console.log("Olá mundo");
+//alert("Olá Mundo!");
 var titulo = document.querySelector(".titulo");
 console.log(titulo);
 console.log(titulo.textContent);
 
 titulo.textContent = "Aparecida Nutricionista";
 
+var paciente = document.querySelector("#primeiro-paciente");
+
+var peso = paciente.querySelector(".info-peso").textContent;
+var altura = paciente.querySelector(".info-altura").textContent;
+
+var imc = peso / (altura * altura);
+
+console.log(peso);
+console.log(altura);
+console.log(imc);
