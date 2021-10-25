d7c98ec062f0d006730cc8922c82601d866a60fd
Alexandre Dos Santos Goncalves <alexandre.sgoncalves@mrv.com.br>
2021-10-25 10:04:46 -0300

    javaScript 04-Eventos, Formulários e Criando Elementos - 06-Adiconando pacientes na tabela

--- javaScript/introducao-javascript/js/principal.js
+++ javaScript/introducao-javascript/js/principal.js
@@ -46,7 +46,35 @@ for (var i = 0; i < pacientes.length; i++) {
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click", function(event){
     event.preventDefault();
-    console.log("botao clicado");
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTd = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    
+    pacienteTd.appendChild(nomeTd);
+    pacienteTd.appendChild(pesoTd);
+    pacienteTd.appendChild(alturaTd);
+    pacienteTd.appendChild(gorduraTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTd);
+
 });
 
 
