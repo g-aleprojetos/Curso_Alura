0da79c7ddfb4bbae2f152d0365a71cae06e30b6c
Alexandre Dos Santos Goncalves <alexandre.sgoncalves@mrv.com.br>
2021-10-25 15:35:56 -0300

    javaScript 07-Remoção, delegação e animação - 06-Animando a remoção do paciente

--- javaScript/introducao-javascript/js/remover-paciente.js
+++ javaScript/introducao-javascript/js/remover-paciente.js
@@ -6,5 +6,10 @@ tabela.addEventListener("dblclick", function(event){
     //var alvoEvento = event.target;
     //var paiDoAlvo = alvoEvento.parentNode;//TR = paciente = remover
     //paiDoAlvo.remove();
-    event.target.parentNode.remove();
+    //event.target.parentNode.remove();
+    event.target.parentNode.classList.add("fadeOut");
+
+    setTimeout(function(){
+        event.target.parentNode.remove();
+    }, 500);
 });
