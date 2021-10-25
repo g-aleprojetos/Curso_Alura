2eade3794d82c4aab477a392b462bc15f9284919
Alexandre Dos Santos Goncalves <alexandre.sgoncalves@mrv.com.br>
2021-10-25 09:40:44 -0300

    javaScript 04-Eventos, Formulários e Criando Elementos - 04-Evitando o comportamento padrão

--- javaScript/introducao-javascript/js/principal.js
+++ javaScript/introducao-javascript/js/principal.js
@@ -44,7 +44,8 @@ for (var i = 0; i < pacientes.length; i++) {
 }
 
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
-botaoAdicionar.addEventListener("click", function(){
+botaoAdicionar.addEventListener("click", function(event){
+    event.preventDefault();
     console.log("botao clicado");
 });
 
