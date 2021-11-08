bf8037b84440735ef6a41742a7507c399aa5a837
Alexandre Dos Santos Goncalves <alexandre.sgoncalves@mrv.com.br>
2021-11-08 13:41:37 -0300

    02-Herança - 02-Herança

--- javaScript/POO-javascript/Conta.js
+++ javaScript/POO-javascript/Conta.js
@@ -1,28 +1,42 @@
-export class Conta{
+export class Conta {
   constructor(saldoInicial, cliente, agencia) {
-      this._saldo = saldoInicial;
-      this._cliente = cliente;
-      this._agencia = agencia;
+    this._saldo = saldoInicial;
+    this._cliente = cliente;
+    this._agencia = agencia;
+  }
+
+  set cliente(novoValor) {
+    if (novoValor instanceof Cliente) {
+      this._cliente = novoValor;
+    }
+  }
+
+  get cliente() {
+    return this._cliente;
+  }
+
+  get _saldo() {
+    return this._saldo;
   }
 
   sacar(valor) {
-      if (this._saldo >= valor) {
-          this._saldo -= valor;
-          return valor;
-      }
+
+    let taxa = 1
+    const valorSacado = taxa * valor;
+    if (this._saldo >= valorSacado) {
+      this._saldo -= valorSacado;
+      return valorSacado;
+    }
   }
 
   depositar(valor) {
-      if (valor <= 100) {
-          return;
-      }
-      this._saldo += valor;
+    this._saldo += valor;
   }
 
   tranferir(valor, conta) {
 
-      const valorSacado = this.sacar(valor);
-      conta.depositar(valorSacado);
+    const valorSacado = this.sacar(valor);
+    conta.depositar(valorSacado);
 
   }
 }
