ae2371eb7a876779a00a7453d12643500f582d65
Alexandre Dos Santos Goncalves <alexandre.sgoncalves@mrv.com.br>
2021-11-08 10:13:56 -0300

    03-Modularizando código - 05-Composição de classes

--- javaScript/POO-javascript/index.js
+++ javaScript/POO-javascript/index.js
@@ -12,13 +12,14 @@ cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
 
 const contaCorrenteRicardo = new ContaCorrente();
-
 contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo.cliente = cliente1;
+contaCorrenteRicardo.depositar(500);
 
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.depositar(100);
-const valorSacado = contaCorrenteRicardo.sacar(50);
+const conta2 = new ContaCorrente();
+conta2.cliente = cliente2;
+conta2.agencia = 102;
 
-console.log(valorSacado);
-console.log(contaCorrenteRicardo)
+contaCorrenteRicardo.transferir(200, conta2);
+console.log(conta2);
+console.log(contaCorrenteRicardo);
