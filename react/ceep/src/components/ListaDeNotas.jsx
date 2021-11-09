84248dd5bdba58e5ab6950be5aa97c6c6617b31a
Alexandre Dos Santos Goncalves <alexandre.sgoncalves@mrv.com.br>
2021-11-09 09:10:19 -0300

    03-Estilizando componentes - 02-Estilizando componentes

--- react/ceep/src/components/ListaDeNotas.jsx
+++ react/ceep/src/components/ListaDeNotas.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
-import CardNota from "./CardNota";
+import CardNota from "./cardNota/CardNota";
 
 export class ListaDeNotas extends Component {
   render() {
     return (
       <ul>
-        {Array.of("Tabalho", "Tabalho", "Estudos").map((categoria) => {
+        {Array.of("Tabalho", "Tabalho", "Estudos").map((categoria, index) => {
           return (
-            <li>
+            <li key={index}>
               <div>{categoria}</div>
               <CardNota />
             </li>
