5c3aa830d1f6cb5832364ae318ecea42c6570b12
Alexandre Dos Santos Goncalves <alexandre.sgoncalves@mrv.com.br>
2021-11-09 11:43:33 -0300

    05-Controlando o estado e a renderização - 04-O state

--- react/ceep/src/components/listaDeNota/ListaDeNotas.jsx
+++ react/ceep/src/components/listaDeNota/ListaDeNotas.jsx
@@ -7,10 +7,10 @@ export class ListaDeNotas extends Component {
   render() {
     return (
       <ul className="lista-notas">
-        {this.props.notas.map((Nota, index) => {
+        {this.props.notas.map((nota, index) => {
           return (
             <li className="lista-notas_item" key={index}>
-              <CardNota />
+              <CardNota titulo={nota.titulo} texto={nota.texto} />
             </li>
           );
         })}
