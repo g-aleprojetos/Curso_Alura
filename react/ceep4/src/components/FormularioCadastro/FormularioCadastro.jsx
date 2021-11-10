43fa99a6078c5641656e8f3cc2402b19b3058f18
Alexandre Dos Santos Goncalves <alexandre.sgoncalves@mrv.com.br>
2021-11-10 15:34:59 -0300

    04 - Contextos e Providers

--- react/ceep4/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ react/ceep4/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,23 +2,23 @@ import React, { useState, useEffect } from "react";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuario";
 import DadosEntrega from "./DadosEntrega";
-import { Step, StepLabel, Stepper, Typography } from "@material-ui/core";
+import { Typography, Stepper, Step, StepLabel } from "@material-ui/core";
 
-function FormularioCadastro({ aoEnviar, validarCPF }) {
+function FormularioCadastro({ aoEnviar, validacoes }) {
   const [etapaAtual, setEtapaAtual] = useState(0);
   const [dadosColetados, setDados] = useState({});
 
   useEffect(() => {
-    if(etapaAtual === formularios.length-1){
+    if (etapaAtual === formularios.length - 1) {
       aoEnviar(dadosColetados);
     }
   });
 
   const formularios = [
-    <DadosUsuario aoEnviar={coletarDados} />,
-    <DadosPessoais aoEnviar={coletarDados} validarCPF={validarCPF} />,
-    <DadosEntrega aoEnviar={coletarDados} />,
-    <Typography variant="h5">Obrigado pelo cadastro!</Typography>
+    <DadosUsuario aoEnviar={coletarDados} validacoes={validacoes} />,
+    <DadosPessoais aoEnviar={coletarDados} validacoes={validacoes} />,
+    <DadosEntrega aoEnviar={coletarDados} validacoes={validacoes} />,
+    <Typography variant="h5">Obrigado pelo Cadastro!</Typography>,
   ];
 
   function coletarDados(dados) {
@@ -28,15 +28,25 @@ function FormularioCadastro({ aoEnviar, validarCPF }) {
   function proximo() {
     setEtapaAtual(etapaAtual + 1);
   }
-  return <>
-  <Stepper activeStep={etapaAtual}>
-    <Step><StepLabel>Login</StepLabel></Step>
-    <Step><StepLabel>Pessoal</StepLabel></Step>
-    <Step><StepLabel>Entrega</StepLabel></Step>
-    <Step><StepLabel>Finalização</StepLabel></Step>
-  </Stepper>
-  {formularios[etapaAtual]}
-  </>;
+  return (
+    <>
+      <Stepper activeStep={etapaAtual}>
+        <Step>
+          <StepLabel>Login</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Pessoal</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Entrega</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Finalização</StepLabel>
+        </Step>
+      </Stepper>
+      {formularios[etapaAtual]}
+    </>
+  );
 }
 
 export default FormularioCadastro;
