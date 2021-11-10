b4a5b3f05913fce7b6bd67cc74fbfdc9b7c2d188
Alexandre Dos Santos Goncalves <alexandre.sgoncalves@mrv.com.br>
2021-11-10 11:02:52 -0300

    04 - Regras do Hooks

--- react/ceep3/src/App.js
+++ react/ceep3/src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from "react";
-import "./App.css";
 import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
+import {Container, Typography } from "@material-ui/core"
+import "./App.css";
 import 'fontsource-roboto';
 
-import {Container, Typography } from "@material-ui/core"
 class App extends Component {
   render() {
     return (
       <Container component="article" maxWidth="sm">
         <Typography variant="h3" component="h1" align="center" >Formulário de cadastro</Typography>
-        <FormularioCadastro />
+        <FormularioCadastro aoEnviar={aoEnviarForm} />
       </Container>
     );
   }
+}
+
+function aoEnviarForm(dados){
+  console.log(dados);
 }
 
 export default App;
