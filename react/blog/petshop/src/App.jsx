b2015f94f260c461cf58e41e89dfddb8f4d0adb0
Alexandre Dos Santos Goncalves <alexandre.sgoncalves@mrv.com.br>
2021-11-11 15:30:17 -0300

    Blog - 04 - Exibindo post e categoria

--- react/blog/petshop/src/App.jsx
+++ react/blog/petshop/src/App.jsx
@@ -6,6 +6,7 @@ import Home from "./paginas/Home";
 import Sobre from "./paginas/Sobre";
 import Pagina404 from "./paginas/Pagina404";
 import Cabecalho from "./components/Cabecalho";
+import Post from "./paginas/Post";
 
 function App() {
   return (
@@ -15,9 +16,15 @@ function App() {
         <Route exact path="/">
           <Home />
         </Route>
+        
         <Route path="/sobre">
           <Sobre />
         </Route>
+
+        <Route path="/posts/:id">
+          <Post />
+        </Route>
+        
         <Route>
           <Pagina404 />
         </Route>
