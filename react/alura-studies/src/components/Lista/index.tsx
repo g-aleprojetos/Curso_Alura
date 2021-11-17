8343fb2ebebf80585390910fb14e81e68384ac59
Alexandre Dos Santos Goncalves <alexandre.sgoncalves@mrv.com.br>
2021-11-17 15:36:55 -0300

    React Typescript - 6 - Selecionando um item

--- react/alura-studies/src/components/Lista/index.tsx
+++ react/alura-studies/src/components/Lista/index.tsx
@@ -2,13 +2,20 @@ import style from "./Lista.module.scss";
 import {ITarefa} from "../../Types/tarefa";
 import Item from "./Item";
 
-function Lista({ tarefas }: { tarefas: ITarefa[] }) {
+interface Props{
+  tarefas: ITarefa[],
+  selecionaTarefa: (tarefaSelecionada: ITarefa) => void
+}
+
+function Lista({ tarefas,selecionaTarefa  }: Props) {
   return (
     <aside className={style.listaTarefas}>
       <h2>Estudo do dia</h2>
       <ul>
-        {tarefas.map((item, index) => (
-          <Item key={index} {...item} />
+        {tarefas.map((item) => (
+          <Item 
+          selecionaTarefa={selecionaTarefa}
+          key={item.id} {...item} />
         ))}
       </ul>
     </aside>
