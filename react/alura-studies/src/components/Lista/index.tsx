6186c3af8cc5676e56e04111b8242a6786c37897
Alexandre Dos Santos Goncalves <alexandre.sgoncalves@mrv.com.br>
2021-11-17 11:24:02 -0300

    React Typescript - 4 - Props

--- react/alura-studies/src/components/Lista/index.tsx
+++ react/alura-studies/src/components/Lista/index.tsx
@@ -1,32 +1,33 @@
-import React from 'react';
-import style from './Lista.module.scss'
-
+import React from "react";
+import style from "./Lista.module.scss";
+import Item from "./Item";
 
 function Lista() {
-  const tarefas = [{
-    tarefa: "React",
-    tempo: "02:00:00"
-  }, {
-    tarefa: "Javascript",
-    tempo: "01:00:00"
-  }, {
-    tarefa: "Typescript",
-    tempo: "03:00:00"
-  }]
+  const tarefas = [
+    {
+      tarefa: "React",
+      tempo: "02:00:00",
+    },
+    {
+      tarefa: "Javascript",
+      tempo: "01:00:00",
+    },
+    {
+      tarefa: "Typescript",
+      tempo: "03:00:00",
+    },
+  ];
 
   return (
-    <aside className={style.listaTarefas} >
+    <aside className={style.listaTarefas}>
       <h2>Estudo do dia</h2>
       <ul>
         {tarefas.map((item, index) => (
-          <li key={index} className={style.item} >
-            <h3>{item.tarefa}</h3>
-            <span>{item.tempo}</span>
-          </li>
+          <Item key={index} {...item} />
         ))}
       </ul>
     </aside>
-  )
+  );
 }
 
 export default Lista;
